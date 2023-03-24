ffe5c61634d7b6091ebffb92b3ba07b74aacf55d
emiryumak <emiryumak@gmail.com>
2023-03-24 16:33:34 +0300

    Fixes

--- projects/ngx-leaflet-measure-path/src/public-api.ts
+++ projects/ngx-leaflet-measure-path/src/public-api.ts
@@ -6,6 +6,6 @@
  * found in the LICENSE file at https://rilke.ist/license
  */
 
-export { LeafletMeasurePathModule } from './lib/leaflet-smooth-zoom.module';
+export { LeafletMeasurePathModule } from './lib/leaflet-measure-path.module';
 export { LeafletMeasurePathDirective } from './lib/core/leaflet-measure-path.directive';
 export * from './lib/core/leaflet-measure-path';
